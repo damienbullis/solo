790723e6b5f277c33bfa9d59d910601688b38c9f
Damien <damien@bodhi.digital>
2022-12-13 19:21:10 -0600

    adds command + keybinding trigger

--- src/extension.ts
+++ src/extension.ts
@@ -4,10 +4,10 @@ import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "solo" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
@@ -15,10 +15,30 @@ export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand("solo.solo", () => {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from SOLO!");
+    const { workspace, window } = vscode;
+    const { workspaceFolders } = workspace;
+    console.log({ vscode, workspaceFolders });
+
+    if (workspaceFolders && workspaceFolders.length > 0) {
+      const { uri } = workspaceFolders[0];
+      const { fsPath } = uri;
+      console.log({ fsPath, uri });
+    }
   });
 
-  context.subscriptions.push(disposable);
+  let active: boolean = false;
+  let mode = vscode.commands.registerCommand("solo.controlMode", () => {
+    const { workspace, window } = vscode;
+    const { workspaceFolders } = workspace;
+    console.log({ workspaceFolders });
+    active = !active;
+    // check if sidebar is open
+    const isSidebarOpen = window.activeTextEditor?.viewColumn === 1;
+    console.log({ isSidebarOpen, active });
+  });
+  
+
+  context.subscriptions.push(disposable, mode);
 }
 
 // This method is called when your extension is deactivated
