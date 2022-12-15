d438cb4ea2d33af8e3bdafe75e9c20d8138cb0c1
Damien <damien@bodhi.digital>
2022-12-15 08:23:55 -0600

    exploring & testing

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const state: { [key: string]: any } = {};
 
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -12,33 +11,37 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("solo.solo", () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    const { workspace, window } = vscode;
-    const { workspaceFolders } = workspace;
-    console.log({ vscode, workspaceFolders });
+  let disposable = vscode.commands.registerCommand("solo", () => {});
 
-    if (workspaceFolders && workspaceFolders.length > 0) {
-      const { uri } = workspaceFolders[0];
-      const { fsPath } = uri;
-      console.log({ fsPath, uri });
-    }
+  let init = vscode.commands.registerCommand("solo.initialize", () => {
+    console.log("solo.initialize");
+    const { workspaceFolders } = vscode.workspace;
+    const files = workspaceFolders?.map((folder) => folder.uri.fsPath);
+    console.log({ files });
+    state["files"] = files;
   });
+
+  // initialize the extension with a execute command solo.initialize
 
   let active: boolean = false;
   let mode = vscode.commands.registerCommand("solo.controlMode", () => {
     const { workspace, window } = vscode;
     const { workspaceFolders } = workspace;
-    console.log({ workspaceFolders });
+
     active = !active;
     // check if sidebar is open
     const isSidebarOpen = window.activeTextEditor?.viewColumn === 1;
-    console.log({ isSidebarOpen, active });
+    console.log({
+      isSidebarOpen,
+      active,
+      state,
+    });
   });
-  
 
-  context.subscriptions.push(disposable, mode);
+  context.subscriptions.push(disposable, init, mode);
+
+  // execute solo.initialize command
+  vscode.commands.executeCommand("solo.initialize");
 }
 
 // This method is called when your extension is deactivated
