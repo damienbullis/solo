c37e1a2874938033e8d7bf658a4d0ae64a9d161c
Damien <damien@bodhi.digital>
2022-12-23 14:51:07 -0600

    testing out explorer context selection

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,71 @@
 import * as vscode from "vscode";
 
-const { log } = console;
-
 const store: {
-  rootFolders: [string, vscode.FileType][];
+  rootFolders: [string, vscode.FileType, vscode.Uri][];
+  workspaceExclude: string[];
+  solodFiles: [string, vscode.Uri][];
 } = {
   rootFolders: [],
+  workspaceExclude: [],
+  solodFiles: [],
+};
+
+const updateExclude = () => {
+  const { workspaceExclude, solodFiles, rootFolders } = store;
+  const filesConfig = vscode.workspace.getConfiguration("files");
+  const next = workspaceExclude.reduce<{ [k: string]: boolean }>(
+    (acc, file) => {
+      acc[file] = true;
+      return acc;
+    },
+    {}
+  );
+
+  const newExclude = rootFolders.reduce<{ [k: string]: boolean }>(
+    (acc, [name, , uri]) => {
+      const checkSolod = solodFiles.find(
+        ([file, _uri]) => uri.path === _uri.path
+      );
+      if (!checkSolod) {
+        acc[name] = true;
+      }
+      return acc;
+    },
+    next
+  );
+  // const newExclude = solodFiles.reduce<{ [k: string]: boolean }>(
+  //   (acc, [name, uri]) => {
+  //     acc[name] = true;
+  //     return acc;
+  //   },
+  //   next
+  // );
+
+  filesConfig.update(
+    "exclude",
+    newExclude,
+    vscode.ConfigurationTarget.Workspace
+  );
 };
 
 const getExcluded = async () => {
   // get current workspace
   const workspace = vscode.workspace.workspaceFolders;
+  console.log({ workspace });
   const path = workspace?.[0].uri;
   // get folders for a given path
   if (path !== undefined) {
-    const folders = await vscode.workspace.fs.readDirectory(
+    const _folders = await vscode.workspace.fs.readDirectory(
       vscode.Uri.from(path)
     );
+    // for each folder map the uri to the name and type
+    const folders = _folders.map((folder) => {
+      const [name, type] = folder;
+      const uri = vscode.Uri.joinPath(path, name);
+      return [name, type, uri];
+    });
 
-    store.rootFolders = folders;
+    store.rootFolders = folders as [string, vscode.FileType, vscode.Uri][];
   }
 };
 
@@ -30,16 +77,12 @@ const currentlyVisible = () => {
   const currentExclude = Object.keys(globalValue || defaultValue || {});
   const workspaceExclude = Object.keys(workspaceValue || {});
   const combinedExclude = [...currentExclude, ...workspaceExclude];
-  // copy workspaceValue to store
-  const filtered = store.rootFolders.filter(([name]) => {
-    return !combinedExclude.find((exclude) => exclude.includes(name));
-  });
+
   Object.assign(store, {
-    workspaceExclude: workspaceValue,
-    filteredFolders: filtered,
+    workspaceExclude: combinedExclude,
   });
 
-  log({ store, currentExclude, filtered });
+  console.log({ store });
 };
 
 export function activate(context: vscode.ExtensionContext) {
@@ -59,22 +102,55 @@ export function activate(context: vscode.ExtensionContext) {
     "solo.mode.disable",
     () => {
       toggleSoloMode();
-      // getExcluded();
       console.log("solo mode disabled");
-      // reset the solo'd files
-      // const filesConfig = vscode.workspace.getConfiguration("files");
-      // filesConfig.update("exclude", {});
     }
   );
 
   const modeEnable = vscode.commands.registerCommand("solo.mode.enable", () => {
     toggleSoloMode();
-    // getExcluded();
     console.log("solo mode enabled");
   });
+  const configOnChange = vscode.workspace.onDidChangeConfiguration((e) => {
+    if (e.affectsConfiguration("files.exclude")) {
+      console.log("do something when exclude changes");
+    }
+  });
+  const test = vscode.commands.registerCommand(
+    "solo.test",
+    async (uri: vscode.Uri) => {
+      // check if uri is a file or folder
+      const { path } = uri;
+      // using fs api to check if path is a file or folder
+      const { type } = await vscode.workspace.fs.stat(uri);
+      // convert type to readable string
+      const uriType = vscode.FileType[type];
+
+      // if (uriType === "Directory") {
+      //   // look through the store rootFolders and filter out any folder that is not the same as the path
+      //   // then add those folders to the exclude list
+      //   const filtered = store.rootFolders.filter((folder) => {
+      //     const [name, , uri] = folder;
+      //     console.log({ name, uri });
+      //     return uri.path !== path;
+      //   });
+      //   // console.log({ filtered });
+      // }
+      const alreadyExcluded = store.solodFiles.find(
+        ([file, uri]) => uri.path === path
+      );
+      if (!alreadyExcluded) {
+        const name = path.split("/").pop();
+        if (name) {
+          store.solodFiles = [...store.solodFiles, [name, uri]];
+        }
+      }
+      updateExclude();
+      console.log({ path, store });
+    }
+  );
 
   // Subscribe to commands
-  context.subscriptions.push(modeDisable, modeEnable);
+  context.subscriptions.push(modeDisable, modeEnable, test, configOnChange);
   getExcluded();
   currentlyVisible();
 
