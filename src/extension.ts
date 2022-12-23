9a686d323c38a94680ff40518c5bad78f0cacc44
Damien <damien@bodhi.digital>
2022-12-22 19:31:56 -0600

    work on folder filtering

--- src/extension.ts
+++ src/extension.ts
@@ -1,39 +1,82 @@
 import * as vscode from "vscode";
 
-const soloContext = {
-  /**
-   * The Mode or State of the extension.
-   * - `true` means that we are in solo mode and we are excluding files
-   * - `false` means that we are not in solo mode and we are seeing all files except the ones that are workspace/settings excluded
-   */
-  soloEnabled: false,
+const { log } = console;
+
+const store: {
+  rootFolders: [string, vscode.FileType][];
+} = {
+  rootFolders: [],
 };
 
-const setSoloMode = (soloMode: boolean) => {
-  soloContext.soloEnabled = soloMode;
-  vscode.commands.executeCommand("setContext", "ext.soloMode", soloMode);
+const getExcluded = async () => {
+  // get current workspace
+  const workspace = vscode.workspace.workspaceFolders;
+  const path = workspace?.[0].uri;
+  // get folders for a given path
+  if (path !== undefined) {
+    const folders = await vscode.workspace.fs.readDirectory(
+      vscode.Uri.from(path)
+    );
+
+    store.rootFolders = folders;
+  }
+};
+
+const currentlyVisible = () => {
+  const filesConfig = vscode.workspace.getConfiguration("files");
+  const res = filesConfig.inspect("exclude");
+  const { defaultValue, globalValue, workspaceValue } = res || {};
+
+  const currentExclude = Object.keys(globalValue || defaultValue || {});
+  const workspaceExclude = Object.keys(workspaceValue || {});
+  const combinedExclude = [...currentExclude, ...workspaceExclude];
+  // copy workspaceValue to store
+  const filtered = store.rootFolders.filter(([name]) => {
+    return !combinedExclude.find((exclude) => exclude.includes(name));
+  });
+  Object.assign(store, {
+    workspaceExclude: workspaceValue,
+    filteredFolders: filtered,
+  });
+
+  log({ store, currentExclude, filtered });
 };
 
 export function activate(context: vscode.ExtensionContext) {
+  const { globalState } = context;
+
+  const toggleSoloMode = () => {
+    const nextMode = !globalState.get("soloMode");
+    // To pass state to package.json
+    vscode.commands.executeCommand("setContext", "ext.soloMode", nextMode);
+    // Internal extension state
+    globalState.update("soloMode", nextMode);
+  };
+
+  // Register commands
+
   const modeDisable = vscode.commands.registerCommand(
     "solo.mode.disable",
     () => {
-      setSoloMode(false);
+      toggleSoloMode();
+      // getExcluded();
+      console.log("solo mode disabled");
       // reset the solo'd files
       // const filesConfig = vscode.workspace.getConfiguration("files");
       // filesConfig.update("exclude", {});
     }
   );
 
-  const modeEnable = vscode.commands.registerCommand("solo.mode.enable", () =>
-    setSoloMode(true)
-  );
+  const modeEnable = vscode.commands.registerCommand("solo.mode.enable", () => {
+    toggleSoloMode();
+    // getExcluded();
+    console.log("solo mode enabled");
+  });
 
   // Subscribe to commands
   context.subscriptions.push(modeDisable, modeEnable);
-
-  // Initialize
-  setSoloMode(soloContext.soloEnabled);
+  getExcluded();
+  currentlyVisible();
 
   // FEATURE: on init check for workspace settings and check if files are excluded
   // if they are then store them in the soloContext, to be restored when solo mode is disabled
@@ -44,6 +87,9 @@ export function activate(context: vscode.ExtensionContext) {
   // then we will start off with those files solo'd and solo mode disabled
 
   // FEATURE: add in a property to the configuration that is the starting state of the extension. Enabled or Disabled
+
+  // FEATURE: add in a profile system that allows you to save a list of files to be excluded and
+  // then you can switch between them, also the property in the configuration for current profile
 }
 
 export function deactivate() {}
